dfbe03e26d488277d3850061fc7186fde4062d5d
Shivanand3394 <career.shivanand@gmail.com>
2026-02-28 10:34:47 +0530

    feat(resume): add resume domain modules (engine/parser/templates/styles)

---
+++ worker/src/domains/resume/templates/index.js
@@ -0,0 +1,7 @@
+import { classicTemplate } from './classic.js';
+import { modernTemplate } from './modern.js';
+
+export const templates = {
+  classic: classicTemplate,
+  modern: modernTemplate
+};
